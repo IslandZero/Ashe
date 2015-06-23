cc89b3cfe2d24b640f331725edae40c69593cbd1
YANKE Guo <me@yanke.io>
2015-06-23 21:17:29 +0800

    add a small url for deleting devices

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def delete_devices
+    Device.delete_all
+
+    render plain: "All Device Data Purged"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  get 'delete_devices' => 'welcome#delete_devices'
+
   resources :bundles, only: [:show]
   resources :builds , only: [:new, :create, :show]
   resources :devices, only: [:new, :create, :show] do
