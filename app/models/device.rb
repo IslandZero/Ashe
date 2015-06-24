025c3b1223a6b766ef45249bdd5516555a1b87e4
YANKE Guo <me@yanke.io>
2015-06-24 14:56:08 +0800

    bugfix

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,14 +2,23 @@ require 'securerandom'
 
 class Device < ActiveRecord::Base
   def self.create_from_plist(plist, owner = nil)
-    device = self.find_or_create_by(udid: plist['UDID'])
+    # Find or create
+    created = false
+    device = self.find_or_create_by(udid: plist['UDID']) do |d|
+      d.token = SecureRandom.urlsafe_base64(32)
+      created = true
+    end
+    # Update device info
     device.update({
       version: plist['VERSION'],
       serial:  plist['SERIAL'],
       product: plist['PRODUCT'],
-      owner:   owner,
-      token:   SecureRandom.urlsafe_base64(32)
+      owner:   owner
     })
+    # Regenerate token if not first time
+    unless created
+      device.update(token: SecureRandom.urlsafe_base64(32))
+    end
     device
   end
 end
